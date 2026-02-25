89b0d98b358d213072414e11f13a6a200d2c0479
Vedika Nair <vedika.l.nair@gmail.com>
2026-02-25 23:20:11 +0530

    small error fix

--- App/src/UseCase1PalindromeCheckerApp.java
+++ App/src/UseCase1PalindromeCheckerApp.java
@@ -1,7 +1,6 @@
 public class UseCase1PalindromeCheckerApp {
-    public class PalindromeCheckerApp {
         public static void main(String[] args) {
             System.out.println("Welcome to Palindrome Checker App");
         }
-    }
 }
+
